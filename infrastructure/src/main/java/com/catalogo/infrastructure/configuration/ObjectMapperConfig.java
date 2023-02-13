855622e6c2b7f4c3c713f14d59a86617ef30c84b
Lucas Reis <lucaschristian10@gmail.com>
2023-02-13 20:08:40 -0300

    feat: configurar ObjectMapper custom

--- infrastructure/src/main/java/com/catalogo/infrastructure/configuration/ObjectMapperConfig.java
+++ infrastructure/src/main/java/com/catalogo/infrastructure/configuration/ObjectMapperConfig.java
@@ -1,2 +1,14 @@
-package com.catalogo.infrastructure.configuration;public class ObjectMapperConfig {
+package com.catalogo.infrastructure.configuration;
+
+import com.catalogo.infrastructure.configuration.json.Json;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    public ObjectMapper objectMapper() {
+        return Json.mapper();
+    }
+
 }
